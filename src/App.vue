8e5b56f484f450f8f9f47bf21956237e03a9f2ff
Wen_Tony <11036696+wen-tony@user.noreply.gitee.com>
2024-03-08 10:44:49 +0800

    创建Vue框架，引入elementplus

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ButtonTest from "./ButtonTest.vue";
+
+// import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <header>
+        <h1>Hello World!</h1>
+        <ButtonTest></ButtonTest>
+    </header>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(ElementPlus, { size: "big", zIndex: 3000 });
+app.mount("#app");
