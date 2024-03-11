705fdda7b339d39e65e4bf4fa4aae80d8ec93b2b
junbo <1428285274@qq.com>
2024-03-11 09:35:14 +0800

    完成推荐接口：输入用户id，得到推荐歌曲mdid列表

---
+++ backend/database.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="172.16.0.176",  # 数据库主机地址
+    user="root",  # 数据库用户名
+    passwd="admin",  # 数据库密码
+    database="minidata"
+)
+cursor = db.cursor()
+
+# ========================
+# tracks
+# ========================
+
+def get_tracks_by_ids(track_ids: list):
+    sql = "SELECT mbid FROM track WHERE id IN (%s)" % ("'" + "','".join(map(str, track_ids)) + "'")
+    # print(sql)
+    cursor.execute(sql)
+    return cursor.fetchall()
--- backend/routers/rec.py
+++ backend/routers/rec.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from ..algorithm import get_recommendation_model
+from ..database import get_tracks_by_ids
 
 router = APIRouter()
 model = get_recommendation_model()
@@ -7,6 +8,8 @@ model = get_recommendation_model()
 
 @router.get("/rec/{userid}", tags=["rec"])
 async def read_user(userid: str):
-    res = model.recall(userid, 10)
-    # print(res)
-    return {"userid": userid, "rec": res[1].tolist()}
+    res = model.recall(userid, 5)
+    track_ids = res[1]
+    track_mbids = get_tracks_by_ids(track_ids)
+    # print(track_mbids, type(track_mbids))
+    return {"userid": userid, "rec": [i[0] for i in track_mbids]}
