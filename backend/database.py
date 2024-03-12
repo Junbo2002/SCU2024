88fe9072216e5b92946964a0000b05b302551f09
Wen_Tony <11036696+wen-tony@user.noreply.gitee.com>
2024-03-12 22:29:52 +0800

    在本地部署看板娘资源，并解决跨域问题

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,12 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="172.16.0.176",  # 数据库主机地址
-    user="root",  # 数据库用户名
-    passwd="admin",  # 数据库密码
-    database="minidata"
-)
-cursor = db.cursor()
+# db = mysql.connector.connect(
+#     host="172.16.0.176",  # 数据库主机地址
+#     user="root",  # 数据库用户名
+#     passwd="admin",  # 数据库密码
+#     database="minidata"
+# )
+# cursor = db.cursor()
 
 # ========================
 # tracks
--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,14 @@ from fastapi import Depends, FastAPI
 # from .dependencies import get_query_token, get_token_header
 # from .internal import admin
 from backend.routers import tracks, rec
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 # app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
 
 app.include_router(tracks.router)
 app.include_router(rec.router)
+app.mount("/static", StaticFiles(directory="assets"), name="static")
 # app.include_router(
 #     admin.router,
 #     prefix="/admin",
@@ -17,6 +19,19 @@ app.include_router(rec.router)
 #     responses={418: {"description": "I'm a teapot"}},
 # )
 
+# 解决跨域问题
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -24,4 +39,4 @@ async def root():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="172.16.0.80", port=8000,)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
