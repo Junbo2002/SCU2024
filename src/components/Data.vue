992d5d715a4e5f46f51370dd664694c0a9b519b8
yy <y2812897116@163.com>
2024-03-13 14:19:10 +0800

    添加滑块组件

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,6 +1,8 @@
 <template>
     <h1>我是数据可视化</h1>
     <WorldMap />
+    <SlideSelect />
+    <StackedBarChartPolar />
     <BarChart />
     <RingChart />
     <LineChart />
@@ -9,6 +11,9 @@
 <script setup>
 import WorldMap from "@/components/graph/WorldMap.vue";
 import BarChart from "@/components/graph/BarChart.vue";
+import SlideSelect from "@/components/graph/SlideSelect.vue";
+import StackedBarChartPolar from "@/components/graph/StackedBarChartPolar.vue";
 import RingChart from "@/components/graph/RingChart.vue";
-import LineChart from "@/components/graph/LineChart.vue"
+import LineChart from "@/components/graph/LineChart.vue";
+
 </script>
---
+++ src/components/graph/StackedBarChartPolar.vue
@@ -0,0 +1,82 @@
+<template>
+    <div
+      ref="StackedBarChartPolar"
+      id="stackBarChartPolar"
+      :style="{ height: '70vh'}"
+    ></div>
+  </template>
+   
+  <script>
+  import { getCurrentInstance, onMounted } from 'vue';
+   
+  export default {
+    setup() {
+      // 通过 internalInstance.appContext.config.globalProperties 获取全局属性或方法
+      let internalInstance = getCurrentInstance();
+      let echarts = internalInstance.appContext.config.globalProperties.$echarts;
+   
+      onMounted(() => {
+        const dom = document.getElementById('stackBarChartPolar');
+        const stackBarChartPolar = echarts.init(dom); // 初始化echarts实例
+        const option = {
+            angleAxis: {},
+            radiusAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu'],
+                z: 10
+            },
+            polar: {},
+            series: [
+                {
+                type: 'bar',
+                color: '#53abd8',
+                data: [1, 2, 3, 4],
+                coordinateSystem: 'polar',
+                name: 'A',
+                stack: 'a',
+                emphasis: {
+                    focus: 'series'
+                }
+                },
+                {
+                type: 'bar',
+                color: '#a4cae5',
+                data: [2, 4, 6, 8],
+                coordinateSystem: 'polar',
+                name: 'B',
+                stack: 'a',
+                emphasis: {
+                    focus: 'series'
+                }
+                },
+                {
+                type: 'bar',
+                color: '#b7dbe3',
+                data: [1, 2, 3, 4],
+                coordinateSystem: 'polar',
+                name: 'C',
+                stack: 'a',
+                emphasis: {
+                    focus: 'series'
+                }
+                }
+            ],
+            legend: {
+                show: true,
+                data: ['A', 'B', 'C']
+            }
+            };
+        // 设置实例参数
+        stackBarChartPolar.setOption(option);
+      });
+      return {};
+    }
+  };
+  </script>
+
+
+<style>
+/* #stackBarChartPolar {
+    background-color: pink;
+} */
+</style>
