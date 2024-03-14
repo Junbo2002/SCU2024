36bfe1faedcc9cf165f849b1d025acdc13b0ae96
yy <y2812897116@163.com>
2024-03-14 14:58:42 +0800

    完成滑块和极坐标柱状图的联动效果

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -3,4 +3,4 @@ export const $p_carousel_item = "#FAFCFF";
 export const $a_carousel_item = "#E4E7EDf0";
 export const $rank_list = "#d9ecff";
 
-export const $base_url = "http://172.16.0.99:8000";
+export const $base_url = "http://172.16.0.49:8000";
--- src/components/Data.vue
+++ src/components/Data.vue
@@ -2,7 +2,6 @@
     <h1>我是数据可视化</h1>
     <WorldMap />
     <StackedBarChartPolar />
-    <SlideSelect />
     <BarChart />
     <RingChart />
     <LineChart />
@@ -11,7 +10,6 @@
 <script setup>
 import WorldMap from "@/components/graph/WorldMap.vue";
 import BarChart from "@/components/graph/BarChart.vue";
-import SlideSelect from "@/components/graph/SlideSelect.vue";
 import StackedBarChartPolar from "@/components/graph/StackedBarChartPolar.vue";
 import RingChart from "@/components/graph/RingChart.vue";
 import LineChart from "@/components/graph/LineChart.vue";
